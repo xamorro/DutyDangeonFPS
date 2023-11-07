8fa2041287b49bc55b569eec820b913247eef757
xamorro <martibp2000@gmail.com>
2023-11-07 23:56:33 +0100

    Recarrega sense acabar

--- Assets/Scripts/scrips fps/ShootController.cs
+++ Assets/Scripts/scrips fps/ShootController.cs
@@ -1,3 +1,5 @@
+using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 public class ShootController : MonoBehaviour
@@ -12,15 +14,37 @@ public class ShootController : MonoBehaviour
 
     [SerializeField] private LayerMask LayerPersonatge;
 
+    [SerializeField] float ForçaImpacte = 4f;
+
+    //BALES I RECARREGUES
+    private int MaxMunicio = 30;
+    private int Municio;
+
     private float nextShootTime = 0f;
+
+    private void Start()
+    {
+        Municio = MaxMunicio;
+    }
+
 
     private void Update()
     {
+        if (Municio <= 0)
+        {
+            Reload();
+            //Es return fa que no continui llegint es codi.
+            return;
+        }
+
+        //Mos dibuixa un laser desde sa posicio de sa camera, cap a nes forward 
         Debug.DrawRay(fpsCamera.transform.position, fpsCamera.transform.forward * range, Color.red);
     }
 
     public void Shoot()
     {
+        Municio--;
+
         if (Time.time >= nextShootTime)
         {
             nextShootTime = Time.time + 1 / shootRate;
@@ -43,14 +67,16 @@ public class ShootController : MonoBehaviour
                 target.DañoRecibido(9);
             }
 
-            //if(hit.transform.TryGetComponent(out Enemy enemic))
-            //{
-            //    enemic.morir
-            //}
+            if (hit.rigidbody != null)
+            {
+                //Aplica força cap enrera de on esteiem fent raycast
+                hit.rigidbody.AddForce(-hit.normal * ForçaImpacte);
+            }
 
+            //Si sa distancia des hit es menor a nes rango que li hem donat, fe lo seguent
             if (hit.distance <= range)
             {
-                //IMPACTE
+                //IMPACTE - Si tenim un objecte de impacte fe aixó
                 if (impactFX != null)
                 {
                     Vector3 bulletDirection = hit.point - fpsCamera.transform.position;
@@ -59,7 +85,14 @@ public class ShootController : MonoBehaviour
                     Destroy(impact, 2f);
                 }
             }
-            
         }
     }
+
+    void Reload()
+    {
+        Debug.Log("Recargando");
+        Municio = MaxMunicio;
+    }
+
+
 }
