7dcacef6115a252952bb30709ff349a9db53528a
xamorro <martibp2000@gmail.com>
2023-11-09 23:50:06 +0100

    Recargando

--- Assets/Scripts/scrips fps/ShootController.cs
+++ Assets/Scripts/scrips fps/ShootController.cs
@@ -11,14 +11,16 @@ public class ShootController : MonoBehaviour
     [SerializeField] Camera fpsCamera;
     [SerializeField] ParticleSystem fireFX;
     [SerializeField] GameObject impactFX;
-
+    
     [SerializeField] private LayerMask LayerPersonatge;
 
     [SerializeField] float ForçaImpacte = 4f;
+    [SerializeField] int tempsreload = 5;
 
     //BALES I RECARREGUES
     private int MaxMunicio = 30;
     private int Municio;
+    private bool Recargant = false;
 
     private float nextShootTime = 0f;
 
@@ -30,22 +32,30 @@ public class ShootController : MonoBehaviour
 
     private void Update()
     {
-        if (Municio <= 0)
+        //Mos dibuixa un laser desde sa posicio de sa camera, cap a nes forward 
+        Debug.DrawRay(fpsCamera.transform.position, fpsCamera.transform.forward * range, Color.red);
+
+        if (Recargant)
         {
-            Reload();
+            return;
+        }
+        else if (Municio <= 0)
+        {
+            //Per poder cridar a un corutin RELOAD q hem creat.
+            StartCoroutine(Reload());
             //Es return fa que no continui llegint es codi.
             return;
         }
 
-        //Mos dibuixa un laser desde sa posicio de sa camera, cap a nes forward 
-        Debug.DrawRay(fpsCamera.transform.position, fpsCamera.transform.forward * range, Color.red);
+
+       
     }
 
     public void Shoot()
     {
         
-
-        if (Time.time >= nextShootTime)
+        //Li pos municio > 0 aqui perque sinó constantment dispara i resta sense importar es reload
+        if (Time.time >= nextShootTime && Municio > 0)
         {
             nextShootTime = Time.time + 1 / shootRate;
             PerformShoot();
@@ -92,10 +102,17 @@ public class ShootController : MonoBehaviour
         }
     }
 
-    void Reload()
+    IEnumerator Reload()
     {
+        Recargant = true;
         Debug.Log("Recargando");
+
+        yield return new WaitForSeconds(tempsreload);
+
         Municio = MaxMunicio;
+        Recargant = false;
+        
+
     }
 
 
