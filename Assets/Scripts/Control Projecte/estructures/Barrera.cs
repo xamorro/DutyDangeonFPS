102b7ca291815292fdaa196b4ba3485927939cf4
xamorro <martibp2000@gmail.com>
2023-11-19 01:19:45 +0100

    Animacio Barrotes

---
+++ Assets/Scripts/Control Projecte/estructures/Barrera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+
+public class Barrera: MonoBehaviour
+{
+    public Animator animator;
+    public Transform player;
+    public Transform barrera;
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.position, barrera.position);
+
+        if (distance <= 10 )
+        {
+            animator.SetBool("Aprop", true);
+        }
+        else
+        {
+            animator.SetBool("Aprop", false);
+        }
+    }
+
+}
