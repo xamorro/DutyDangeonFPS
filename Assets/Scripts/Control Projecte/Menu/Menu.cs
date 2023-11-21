d9b382be14ada9e22175642c0d8c35deada93c87
xamorro <martibp2000@gmail.com>
2023-11-21 19:22:51 +0100

    Menu SCENE

--- Assets/Scripts/Control Projecte/Menu/Menu.cs
+++ Assets/Scripts/Control Projecte/Menu/Menu.cs
@@ -2,46 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    
+    //Aixo auria de estar a nes sound manager.
+    private float soundVolume;
+    [SerializeField] TMP_Text textSoundVolume;
+    [SerializeField] Slider soundSlider;
+
+
+
+    [SerializeField] private GameObject principalMenu;
     [SerializeField] private GameObject optMenu;
     [SerializeField] private GameObject aboutMenu;
 
+    private void Start()
+    {
+        //crida s'script quan es valor de slider cambia
+        soundSlider.onValueChanged.AddListener(delegate { setSoundVolume(); });
+    }
 
     public void StartGame()
     {
-        //SceneManager.LoadScene(4);
+        SceneManager.LoadScene(1);
         Debug.Log("Cargando Joc");
     }
 
 
-    void CloseMenuOptions()
+    public void CloseMenuOptions()
     {
         optMenu.SetActive(false);
+        principalMenu.SetActive(true);
     }
 
-    void CloseMenuAbout()
+    public void CloseMenuAbout()
     {
         aboutMenu.SetActive(false);
+        principalMenu.SetActive(true);
     }
 
-    void OpenMenuOptions()
+    public void OpenMenuOptions()
     {
         optMenu.SetActive(true);
+        principalMenu.SetActive(false);
     }
 
-    void OpenMenuAbout()
+    public void OpenMenuAbout()
     {
-        aboutMenu.SetActive(false);
+        aboutMenu.SetActive(true);
     }
 
-    void ExitGame()
+    public void ExitGame()
     {
         Application.Quit();
         Debug.Log("Exit Game");
     }
 
 
+
+
+    public void setSoundVolume()
+    {
+        //Aqui auriem de agafar es valor des sound manager. Ara es una prova
+        soundVolume = soundSlider.value;
+        textSoundVolume.text = soundVolume.ToString();
+
+    }
 
 }
